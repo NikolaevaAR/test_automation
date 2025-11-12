bb7b1b6e7a18bb21eb1cf326f449000372f615dc
NikolaevaAR <nikolanastya2611@gmail.com>
2025-11-12 21:18:11 +0300

    добавлены решения по уроку 2.4

---
+++ lesson2_4_step6.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+try:
+	browser = webdriver.Chrome()
+	browser.get("http://suninjuly.github.io/cats.html")
+	browser.implicitly_wait(5)
+	
+	button = browser.find_element(By.ID, "button")
+	button.click()
+	
+finally:
+	browser.quit()
+	
+	
